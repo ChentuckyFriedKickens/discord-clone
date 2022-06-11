26872da96fb0ba220c9669fa45c3455824e73e62
Laeeq <laeeqhaq@gmail.com>
2022-06-11 15:16:20 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './**.html',
+    './*/**{.html,tsx,ts}',
+    './src/components/**{.tsx,jsx,ts,js}'
+  ],
+  theme: {
+    colors: {
+      blurple : '#404eed',
+      bright : '#fff',
+      dark : '#23272a',
+      offWhite : '#f6f6f6',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
