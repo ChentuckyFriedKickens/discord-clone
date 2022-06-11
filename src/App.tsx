f487d627db4eae3a9ead85136527005ca154c058
Laeeq <laeeqhaq@gmail.com>
2022-06-11 16:24:33 +0600

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,14 @@ const Navbar = () => {
 
   const Navbutton = (props : any) => {
     return (
-      <a className='font-semibold mx-5 px-1 hover:cursor-pointer hover:underline' href={props.link}>{props.text}</a>
+      <a className='font-semibold mx-4 px-1 hover:cursor-pointer hover:underline' href={props.link}>{props.text}</a>
     )
   }
 
   return (
-    <nav className='flex flex-row items-center justify-around p-5'>
+    <nav className='flex flex-row items-center justify-between p-5 w-4/6'>
       <Logo />
-      <div className='flex flex-row items-center justify-center'>
+      <div className='flex flex-row items-center justify-center text-sm'>
         <Navbutton text='Download' />
         <Navbutton text='Nitro' />
         <Navbutton text='Safety' />
@@ -38,7 +38,7 @@ const Navbar = () => {
 
 const Button = (props : any) => {
   return (
-    <a className={`${props.elClasses}`} href={props.link}>
+    <a className={` mx-2 ${props.elClasses}`} href={props.link}>
       {props.text}
     </a>
   )
@@ -48,15 +48,44 @@ const Destructure = () => {
   return (
     <div>
       <section className='heroSection bg-blurple minh-40 text-bright'>
-        <Navbar />
-        <div className='px-48'>
+        <div className='flex justify-center items-center'>
+          <Navbar />
+        </div>
+        <div className='px-44'>
           <article className='text-center px-96 pt-32 flex flex-col justify-center'>
             <h1 className='text-bright'>IMAGINE A PLACE...</h1>
             <br />
             <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
-            <div></div>
+            <div className='flex flex-row items-center justify-center text-xl p-6'>
+              <Button elClasses='light-btn' text='Download' />
+              <Button elClasses='dark-btn' text='Open Discord in your browser' />
+            </div>
           </article>
         </div>
+      </section>
+      <section className='bg-white flex flex-row justify-center py-32'>
+        <img src="../images/section-image-1.svg" className='w-1/3' />
+        <article className='w-1/5 mx-20 pt-6'>
+          <h2>
+            Create an <br /> invite-only <br /> place where you <br /> belong
+          </h2>
+          <br />
+          <p>
+            Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.
+          </p>
+        </article>
+      </section>
+      <section>
+        <article className='w-1/5 mx-20 pt-6'>
+          <h2>
+            Create an <br /> invite-only <br /> place where you <br /> belong
+          </h2>
+          <br />
+          <p>
+            Discord servers are organized into topic-based channels where you can collaborate, share, and just talk about your day without clogging up a group chat.
+          </p>
+        </article>
+        <img src="../images/section-image-2.svg" className='w-1/3' />
       </section>
     </div>
   )
