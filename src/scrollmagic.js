07801e7d9dd293555128fec103483ba922a8794a
Laeeq <laeeqhaq@gmail.com>
2022-06-14 11:04:09 +0600

    added scroll animations

--- src/scrollmagic.js
+++ src/scrollmagic.js
@@ -1,6 +1,10 @@
-//A script to animate all elements with a certain class
+//A script to fade in all elements with a certain class upon reaching them on scroll
 
+//Note: You must have the cdn link for scrollmagic at the end of the body and a jquery cdn in the end of the head in your html as well as a css class "show"
+
+//Put the name of the variable/element you want to animate on scroll
 let targetName = '.animate'
+let reverse = false;
 
 import ScrollMagic from "scrollmagic";
 
@@ -15,15 +19,13 @@ $(document).ready(function() {
       triggerElement : element
     })
     .setClassToggle(element, 'show')
-    .reverse(false)
+    .reverse(reverse)
     .addTo(controller);
   }
-    
-  Animate(targets[0]);
-  Animate(targets[1]);
-  Animate(targets[2]);
-  Animate(targets[3]);  
+  
+  const testArr = [1,2,3,4,5]
 
-  for (var i in targets.length) {
+  for (let i = 0; i < targets.length; i++) {
+    Animate(targets[i])
   }
 })
